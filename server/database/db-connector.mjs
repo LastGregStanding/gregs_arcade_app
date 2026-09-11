054027802930f2f9522b53acd400d6da80430e69
LastGregStanding <LastGregStanding@gmail.com>
2026-09-11 15:44:49 -0600

    use database_url connection string instead of separate postgres env vars

--- server/database/db-connector.mjs
+++ server/database/db-connector.mjs
@@ -6,11 +6,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   max: 10,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT || 5432,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
